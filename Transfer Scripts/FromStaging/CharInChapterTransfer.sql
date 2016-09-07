839a2913b6c04339196ba28e8560622d3658b9e8
Julian Koberg <Kokweazel@Julians-MBP.fritz.box>
2016-09-07 23:51:27 +0200

    Major Changes

--- Transfer Scripts/FromStaging/CharInChapterTransfer.sql
+++ Transfer Scripts/FromStaging/CharInChapterTransfer.sql
@@ -1,52 +1,83 @@
-/* Insert new Characters */
-INSERT INTO prod."D_Character" ("FirstName", "SurName") 
-    SELECT DISTINCT
-        sta."charfirstname"
-        , sta."charsurname"
-    FROM staging."charinchapter" sta
+/* Get Last Loaded Info */
+WITH LastLoaded AS (
+    SELECT
+        *
+        , ROW_NUMBER() OVER (PARTITION BY ("charfirstname", "charsurname")
+                             ORDER BY "insertiontime" DESC) AS rn
+    FROM
+        staging."charinchapter"
+    )
 
-    WHERE NOT EXISTS (
-        SELECT 1 
-        FROM prod."D_Character" ch
-        WHERE ch."FirstName" = sta."charfirstname"
-          AND ch."SurName" = sta."charsurname"
-    );
+/* InsertNewCharacters */
+    INSERT INTO prod."D_Character" ("FirstName", "SurName") 
+        SELECT
+            sta."charfirstname"
+            , sta."charsurname"
+        FROM LastLoaded sta
+        LEFT JOIN prod."D_Character" ch ON ch."FirstName" = sta."charfirstname"
+                                        AND ch."SurName" = sta."charsurname"
 
-/* Insert new Books */
-INSERT INTO prod."D_Book" ("Name") 
-    SELECT DISTINCT
-        sta."bookname"
-    FROM staging."charinchapter" sta
+        WHERE sta."rn" = 1
+          AND ch."FirstName" IS NULL
 
-    WHERE NOT EXISTS (
-        SELECT 1 
-        FROM prod."D_Book" bk
-        WHERE bk."Name" = sta."bookname"
-    );
+; WITH LastLoaded AS (
+    SELECT
+        *
+        , ROW_NUMBER() OVER (PARTITION BY ("bookname")
+                             ORDER BY "insertiontime" DESC) AS rn
+    FROM
+        staging."charinchapter"
+    )
+/* InsertNewBooks */
+    INSERT INTO prod."D_Book" ("Name") 
+        SELECT
+            sta."bookname"
+        FROM LastLoaded sta
+        LEFT JOIN prod."D_Book" bk ON bk."Name" = sta."bookname"
 
+        WHERE sta."rn" = 1
+          AND bk."Name" IS NULL
 
-/* Insert new Chapters */
-INSERT INTO prod."D_Chapter" ("BookId", "ChapterOfBookNumber")
-    SELECT DISTINCT
-        bk."Id"
-        , sta."chapternumber"
-    FROM 
-        staging."charinchapter" sta
+; WITH LastLoaded AS (
+    SELECT
+        *
+        , ROW_NUMBER() OVER (PARTITION BY ("bookname", "chapternumber")
+                             ORDER BY "insertiontime" DESC) AS rn
+    FROM
+        staging."charinchapter"
+    )
+
+/* InsertNewChapters */
+INSERT INTO prod."D_Chapter" ("ChapterOfBookNumber", "BookId", "Name", "MainCharacterId")
+    SELECT
+        sta."chapternumber"
+        , bk."Id"
+        , sta."chaptername"
+        , ch."Id"
+    FROM LastLoaded sta
     INNER JOIN prod."D_Book" bk ON bk."Name" = sta."bookname"
+    INNER JOIN prod."D_Character" ch ON ch."FirstName" = sta."charfirstname"
+                                    AND ch."SurName" = sta."charsurname"
+    LEFT JOIN prod."D_Chapter" cp ON cp."ChapterOfBookNumber" = sta."chapternumber"
+                                  AND cp."BookId" = bk."Id"
+    WHERE sta."rn" = 1
+      AND cp."ChapterOfBookNumber" IS NULL
 
-    WHERE NOT EXISTS (
-        SELECT 1 
-        FROM prod."D_Chapter" ch
-        WHERE ch."ChapterOfBookNumber" = sta."chapternumber"
-          AND ch."BookId" = bk."Id"
-    );
+; WITH LastLoaded AS (
+    SELECT
+        *
+        , ROW_NUMBER() OVER (PARTITION BY ("bookname", "chapternumber", "charfirstname", "charsurname")
+                             ORDER BY "insertiontime" DESC) AS rn
+    FROM
+        staging."charinchapter"
+    )
 
 /* Insert Fact */
 INSERT INTO prod."F_PovCharacterInChapter" ("CharacterId", "ChapterId")
     SELECT
         ch."Id"
         , cp."Id"
-    FROM staging."charinchapter" sta
+    FROM LastLoaded sta
 
     INNER JOIN prod."D_Character" ch ON ch."FirstName" = sta."charfirstname"
                                     AND ch."SurName" = sta."charsurname"
@@ -55,12 +86,10 @@ INSERT INTO prod."F_PovCharacterInChapter" ("CharacterId", "ChapterId")
 
     INNER JOIN prod."D_Chapter" cp ON cp."ChapterOfBookNumber" = sta."chapternumber"
                                   AND cp."BookId" = bk."Id"
-    WHERE NOT EXISTS (
-        SELECT 1
-        FROM prod."F_PovCharacterInChapter" pov
-        WHERE pov."ChapterId" = ch."Id"
-          AND pov."ChapterId" = cp."Id"
-        );
+    LEFT JOIN prod."F_PovCharacterInChapter" pov ON pov."CharacterId" = ch."Id"
+                                                AND pov."ChapterId" = cp."Id"
+    WHERE sta."rn" = 1
+      AND pov."ChapterId" IS NULL
 
 
 
