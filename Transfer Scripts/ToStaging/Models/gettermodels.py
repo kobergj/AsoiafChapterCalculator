8e8de2766364af1c2cdd532aa01e08ede2fd92c4
Julian Koberg <Kokweazel@Julians-MBP.fritz.box>
2016-09-13 21:29:10 +0200

    GetterModels

---
+++ Transfer Scripts/ToStaging/Models/gettermodels.py
@@ -0,0 +1,11 @@
+
+class Character:
+    def __init__(self, name, gender, born, died, father, culture, mother, spouse): 
+        self.name = name 
+        self.gender = gender 
+        self.born = born 
+        self.died = died 
+        self.father = father 
+        self.culture = culture 
+        self.mother = mother 
+        self.spouse = spouse
