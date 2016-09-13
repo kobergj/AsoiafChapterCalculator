806bd19c4d39433fdbab608bc0c96c6c1f3ecb01
Julian Koberg <Kokweazel@Julians-MBP.fritz.box>
2016-09-13 23:31:20 +0200

    Culture

--- recreatetables.sh
+++ recreatetables.sh
@@ -1,6 +1,6 @@
 psql AsoiafDWH -f Tables/F_EventInChapter.sql
-
 psql AsoiafDWH -f Tables/D_Character.sql
 psql AsoiafDWH -f Tables/D_Chapter.sql
 psql AsoiafDWH -f Tables/D_Book.sql
 psql AsoiafDWH -f Tables/D_Gender.sql
+psql AsoiafDWH -f Tables/D_Culture.sql
